5ce1bb716ea072e3169e570a207163459266dcae
guokai.embers <guokai.embers@bytedance.com>
2023-04-02 21:02:07 +0800

    update code

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ import styles from './style.module.less';
 
 export default function HomePage() {
   return (
-    <div className={styles.page}>
+    <div className={styles.Page}>
       <CanvasBg type="universe" />
       <div className={styles.header}>
         <div className={styles.headerTitle}>宇宙大屏</div>
