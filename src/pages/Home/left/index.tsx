a12d327814d2f69cc158d16917df54d97e31f445
guokai.embers <guokai.embers@bytedance.com>
2023-04-08 18:40:53 +0800

    update code

--- src/pages/Home/left/index.tsx
+++ src/pages/Home/left/index.tsx
@@ -1,37 +1,41 @@
 import { ScrollBoard } from '@jiaminghi/data-view-react';
-import { useEffect, useMemo, useState } from 'react';
 
 import BoardHeader from '@/components/BoardHeader';
-import { toAdaptedPx } from '@/utils';
-import { useWindowSize } from '@/utils/useWindowSize';
 
 import styles from './style.module.less';
 
 export default function Left(props) {
     const { health, nature } = props;
 
-    const windowSize = useWindowSize();
-    // const update = useUpdate();
+    const healthConfig = {
+        data:
+            health?.map((item) => {
+                return [
+                    `<span style="height:10px;">${item.sortNum}</span>`,
+                    `<span style="height:10px;">${item.countryName}(${item.countryNameEn})</span>`,
+                    `<span style="height:10px;">${item.score?.toFixed(2)}</span>`,
+                ];
+            }) ?? [],
+        oddRowBGC: 'rgba(255, 255, 255, 0.17)',
+        evenRowBGC: 'transparent',
+        columnWidth: [100, 240, 100],
+        rowNum: 8,
+    };
 
-    const [healthConfig, setHealthConfig] = useState({});
-    useEffect(() => {
-        setHealthConfig(
-            structuredClone({
-                data:
-                    health?.map((item) => {
-                        return [
-                            `<span style="height:10px;">${item.sortNum}</span>`,
-                            `<span style="height:10px;">${item.countryName}(${item.countryNameEn})</span>`,
-                            `<span style="height:10px;">${item.score?.toFixed(2)}</span>`,
-                        ];
-                    }) ?? [],
-                oddRowBGC: 'rgba(255, 255, 255, 0.17)',
-                evenRowBGC: 'transparent',
-                columnWidth: [100, 240, 100],
-                rowNum: 8
-            }),
-        );
-    }, [health, windowSize]);
+    const natureConfig = {
+        data:
+            nature?.map((item) => {
+                return [
+                    `<span style="height:10px;">${item.sortNum}</span>`,
+                    `<span style="height:10px;">${item.countryName}(${item.countryNameEn})</span>`,
+                    `<span style="height:10px;">${item.score?.toFixed(2)}</span>`,
+                ];
+            }) ?? [],
+        oddRowBGC: 'rgba(255, 255, 255, 0.17)',
+        evenRowBGC: 'transparent',
+        columnWidth: [100, 240, 100],
+        rowNum: 8,
+    };
 
     return (
         <div className={styles.Left}>
@@ -39,7 +43,10 @@ export default function Left(props) {
                 <BoardHeader></BoardHeader>
                 <ScrollBoard config={healthConfig} />
             </div>
-            <div></div>
+            <div>
+                <BoardHeader borderColor="#E07B29"></BoardHeader>
+                <ScrollBoard config={natureConfig} />
+            </div>
         </div>
     );
 }
