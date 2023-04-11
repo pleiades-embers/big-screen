2981437350003ff9c885610faaca1887fb977a6e
guokai.embers <guokai.embers@bytedance.com>
2023-04-11 18:47:57 +0800

    update code

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,14 +1,14 @@
 export default {
   local: {
-    BASE_API: 'https://big-screen-sigma.vercel.app/',
+    BASE_API: 'https://big-screen-sigma.vercel.app',
   },
   dev: {
-    BASE_API: 'https://big-screen-sigma.vercel.app/',
+    BASE_API: 'https://big-screen-sigma.vercel.app',
   },
   test: {
-    BASE_API: 'https://big-screen-sigma.vercel.app/',
+    BASE_API: 'https://big-screen-sigma.vercel.app',
   },
   prod: {
-    BASE_API: 'https://big-screen-sigma.vercel.app/',
+    BASE_API: 'https://big-screen-sigma.vercel.app',
   },
 }[VITE_MODE];
