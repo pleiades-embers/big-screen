26157eceb28f52f6474c40fbedff704c19c31fec
guokai.embers <guokai.embers@bytedance.com>
2023-04-14 01:24:01 +0800

    update code

--- src/pages/Home/center/index.tsx
+++ src/pages/Home/center/index.tsx
@@ -97,68 +97,64 @@ export default function Center(props) {
     );
 }
 
-const mapOption = {
-    series: {
-        boxWidth: 110, //三维地图的宽度
-        boxDepth: 80, //地图倾斜度  正交投影失效
-        regionHeight: 0.1, //地图厚度
-        itemStyle: {
-            opacity: 1, // 透明度
-            borderWidth: 0.6, //分界线宽度
-            borderColor: '#207fce', //分界线颜色
-        },
-        groundplane: {
-            show: false,
-        },
-        light: {
-            main: {
-                intensity: 3, //光照强度
-                alpha: 24,
-                shadow: true,
-                shadowQuality: 'high',
-            },
-            ambient: {
-                intensity: 0,
-            },
-        },
-        shading: 'realistic',
-        // 真实感材质相关配置 shading: 'realistic'时有效
-        realisticMaterial: {
-            detailTexture: '#fff', // 纹理贴图
-            textureTiling: 1, // 纹理平铺，1是拉伸，数字表示纹理平铺次数
-            roughness: 0.5, // 材质粗糙度，0完全光滑，1完全粗糙
-            metalness: 0, // 0材质是非金属 ，1金属
-            roughnessAdjust: 0,
-        },
-        viewControl: {
-            // 投影方式，默认为透视投影'perspective'，也支持设置为正交投影'orthographic'。
-            // projection: 'orthographic',
-            // 正交投影此配置失效
-            distance: 85,
-            orthographicSize: 90,
-            animation: false,
-            panMouseButton: 'left',
-            rotateMouseButton: 'middle',
-            center: [0, 5, 0],
-        },
-    },
-    //处理特效
-    postEffect: {
-        enable: true,
-        //环境光遮蔽
-        screenSpaceAmbientOcclusion: {
-            enable: true,
-            quality: 'high',
-        },
-        bloomObject: {
-            enable: true,
-        },
-        //分帧超采样
-        temporalSuperSampling: {
-            enable: true,
-        },
-    },
-};
+// const mapOption = {
+//     series: {
+//         boxWidth: 110, //三维地图的宽度
+//         boxDepth: 80, //地图倾斜度  正交投影失效
+//         regionHeight: 0.1, //地图厚度
+
+//         groundplane: {
+//             show: false,
+//         },
+//         light: {
+//             main: {
+//                 intensity: 3, //光照强度
+//                 alpha: 24,
+//                 shadow: true,
+//                 shadowQuality: 'high',
+//             },
+//             ambient: {
+//                 intensity: 0,
+//             },
+//         },
+//         shading: 'realistic',
+//         // 真实感材质相关配置 shading: 'realistic'时有效
+//         realisticMaterial: {
+//             detailTexture: '#fff', // 纹理贴图
+//             textureTiling: 1, // 纹理平铺，1是拉伸，数字表示纹理平铺次数
+//             roughness: 0.5, // 材质粗糙度，0完全光滑，1完全粗糙
+//             metalness: 0, // 0材质是非金属 ，1金属
+//             roughnessAdjust: 0,
+//         },
+//         viewControl: {
+//             // 投影方式，默认为透视投影'perspective'，也支持设置为正交投影'orthographic'。
+//             // projection: 'orthographic',
+//             // 正交投影此配置失效
+//             distance: 85,
+//             orthographicSize: 90,
+//             animation: false,
+//             panMouseButton: 'left',
+//             rotateMouseButton: 'middle',
+//             center: [0, 5, 0],
+//         },
+//     },
+//     //处理特效
+//     postEffect: {
+//         enable: true,
+//         //环境光遮蔽
+//         screenSpaceAmbientOcclusion: {
+//             enable: true,
+//             quality: 'high',
+//         },
+//         bloomObject: {
+//             enable: true,
+//         },
+//         //分帧超采样
+//         temporalSuperSampling: {
+//             enable: true,
+//         },
+//     },
+// };
 function getChart(data, visualMap): ECOption {
     return {
         tooltip: {
@@ -220,6 +216,11 @@ function getChart(data, visualMap): ECOption {
                     project: (point) => [point[0] / 180 * Math.PI, -Math.log(Math.tan((Math.PI / 2 + point[1] / 180 * Math.PI) / 2))],
                     unproject: (point) => [point[0] * 180 / Math.PI, 2 * 180 / Math.PI * Math.atan(Math.exp(point[1])) - 90]
                 },
+                top: toAdaptedPx(180),
+                height: toAdaptedPx(450),
+                left: toAdaptedPx(10),
+                right: toAdaptedPx(70),
+                bottom: toAdaptedPx(0),
                 label: {
                     show: true,
                     // @ts-ignore
@@ -242,7 +243,12 @@ function getChart(data, visualMap): ECOption {
                         }
                     }
                 },
-                ...mapOption.series,
+                itemStyle: {
+                    opacity: 1, // 透明度
+                    borderWidth: 0.6, //分界线宽度
+                    borderColor: '#207fce', //分界线颜色
+                },
+                // ...mapOption.series,
             },
 
         ],
