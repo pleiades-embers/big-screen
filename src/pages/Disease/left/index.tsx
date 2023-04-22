a10414a6d2f8b650594d43088490a1c80be8e504
guokai.embers <guokai.embers@bytedance.com>
2023-04-22 22:59:22 +0800

    排名轮播

--- src/pages/Disease/left/index.tsx
+++ src/pages/Disease/left/index.tsx
@@ -1,22 +1,22 @@
-import { useRequest } from "ahooks";
+import { useRequest } from 'ahooks';
 
-import RankPng from "@/assets/disease/rank.png"
-import WorldCloudTitlePng from "@/assets/disease/world-cloud-title.png"
-import type { ECOption } from "@/components/SuperEChart";
-import { SuperEChart } from "@/components/SuperEChart";
-import { getCityRankByWord, getWordsCloud } from "@/services/disease";
+import RankPng from '@/assets/disease/rank.png';
+import WorldCloudTitlePng from '@/assets/disease/world-cloud-title.png';
+import { AutoScrollView } from '@/components/AutoScrollView';
+import type { ECOption } from '@/components/SuperEChart';
+import { SuperEChart } from '@/components/SuperEChart';
+import { getCityRankByWord, getWordsCloud } from '@/services/disease';
 
-import styles from "./style.module.less"
-
+import styles from './style.module.less';
 
 export default function Left() {
-
     /**
      * 词云
      */
     const { data } = useRequest(getWordsCloud);
-    const { data: dataRank } = useRequest(() => getCityRankByWord("influenza"));
-    console.log(dataRank, "dataRank")
+    const { data: dataRank } = useRequest(() => getCityRankByWord('influenza'));
+    console.log(dataRank, 'dataRank');
+
     return (
         <div className={styles.left}>
             <div>
@@ -25,41 +25,58 @@ export default function Left() {
             </div>
             <div>
                 <img src={RankPng} alt="" />
-
+                <div className={styles.list} >
+                    <AutoScrollView height={52 * 10} mode="step" stepHeight={52}>
+                        {(dataRank ?? []).map((el) => {
+                            return (
+                                <div className={styles.listItem} key={el.countryName}>
+                                    <div>
+                                        <div className={styles.title}>{el.sortNum}.</div>
+                                        <div className={styles.title}>{el.countryName}</div>
+                                        <div className={styles.value}>{el.value ?? 0}</div>
+                                    </div>
+                                    <div className={styles.progressBar}>
+                                        <div style={{ width: `${el.value}%` }}></div>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </AutoScrollView>
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 function getChart(data): ECOption {
     return {
-
-        series: [{
-            //@ts-ignore
-            type: 'wordCloud',
-            shape: 'circle',
-            keepAspect: false,
-            width: '94%',
-            height: '90%',
-            right: "3%",
-            left: "3%",
-            top: "0%",
-            bottom: "10%",
-            sizeRange: [18, 36],
-            rotationRange: [0, 0],
-            gridSize: 16,
-            textStyle: {
-                // fontFamily: 'PingFangSC',
-                fontFamily: 'AlibabaPuHuiTi-2',
-                fontWeight: 'bold',
-                color: function () {
-                    let colors = ['#46DADB', "#3254DD", "#BEE5FB", "#C6FFAA"]
-                    //@ts-ignore
-                    return colors[parseInt(Math.random() * 4)];
+        series: [
+            {
+                //@ts-ignore
+                type: 'wordCloud',
+                shape: 'circle',
+                keepAspect: false,
+                width: '94%',
+                height: '90%',
+                right: '3%',
+                left: '3%',
+                top: '0%',
+                bottom: '10%',
+                sizeRange: [18, 36],
+                rotationRange: [0, 0],
+                gridSize: 16,
+                textStyle: {
+                    // fontFamily: 'PingFangSC',
+                    fontFamily: 'AlibabaPuHuiTi-2',
+                    fontWeight: 'bold',
+                    color: function () {
+                        let colors = ['#46DADB', '#3254DD', '#BEE5FB', '#C6FFAA'];
+                        //@ts-ignore
+                        return colors[parseInt(Math.random() * 4)];
+                    },
                 },
+                data: data ?? [],
             },
-            data: data ?? []
-        }],
-
-    }
+        ],
+    };
 }
